f051eb65963d188f3f2a7a773ee8c20f10384719
tsuccess <taofeeq.muhammad22@gmail.com>
2025-11-21 15:44:53 +0100

    hello update

--- resources/views/layouts/dashboardtemp.blade.php
+++ resources/views/layouts/dashboardtemp.blade.php
@@ -28,8 +28,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    @vite(['resources/css/dashboard.css'])
-    @vite(['resources/css/access.css'])
+    @vite(['resources/css/dashboard.css','resources/css/access.css'])
+
 
 </head>
 
