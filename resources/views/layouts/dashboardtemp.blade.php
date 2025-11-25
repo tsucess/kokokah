af3609f0d913b9e00e2e97ef703039b332c24ca4
tsuccess <taofeeq.muhammad22@gmail.com>
2025-11-25 12:31:41 +0100

    style linked

--- resources/views/layouts/dashboardtemp.blade.php
+++ resources/views/layouts/dashboardtemp.blade.php
@@ -19,16 +19,13 @@
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
-    <link href="css/style.css" rel="stylesheet">
-
-
     <!-- Inter font -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
-
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
+    
+    
+     <link rel="stylesheet" href="{{ asset('css/style_theme.css') }}">
      <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
     <link rel="stylesheet" href="{{ asset('css/access.css') }}">
     {{-- @vite(['resources/css/dashboard.css','resources/css/access.css']) --}}
