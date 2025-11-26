9cd52578135ef36e24bd3771c6e6240c0fcf6e8d
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2025-11-26 17:46:54 +0100

    added sms page

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -24,7 +24,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/responsiveness.css') }}">
 
     {{-- @vite(['resources/css/style.css', 'resources/css/responsiveness.css']) --}}
@@ -37,6 +37,8 @@
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
+    {{-- swiper.js --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css" />
 </head>
 
 <body>
@@ -117,7 +119,7 @@
                     <div class="input-group">
                         <input type="email" class="form-control newsletter-input p-3" placeholder="Enter your email"
                             aria-label="Enter your email">
-                        <button class="btn fw-bold newsletter-button" type="button" >Subscribe Now</button>
+                        <button class="btn fw-bold newsletter-button" type="button">Subscribe Now</button>
                     </div>
                 </div>
             </div>
--- resources/views/sms.blade.php
+++ resources/views/sms.blade.php
@@ -1,55 +1,279 @@
 @extends('layouts.template')
 
 @section('content')
-    <div class="container-fluid   pt-5" style="background-color:#CCDBDD;">
-        <div class="row align-items-center text-center position-relative">
-            <!-- Main Content -->
-            <div class="col-md-12 text-center mx-auto">
-                <h1 class = "heroheading">
-                    We’re Building to Serve You Better
-                </h1>
+    <section>
+        <section class="container-fluid sms-hero">
+            <div class="row py-5 px-3 px-lg-5 g-5">
+                <div class="col-12">
+                    <div class="d-flex flex-column gap-4 align-items-center">
+                        <div class="d-flex flex-column gap-3 align-items-center">
+                            <div class="d-flex flex-column gap-1 align-items-center">
+                                <div class="d-flex flex-column flex-md-row gap-2 align-items-center">
+                                    <div class="d-flex align-items-center gap-1">
+                                        <i class="fa-solid fa-star fa-2xs" style="color:#FDAF22;"></i>
+                                        <i class="fa-solid fa-star fa-2xs" style="color:#FDAF22;"></i>
+                                        <i class="fa-solid fa-star fa-2xs" style="color:#FDAF22;"></i>
+                                        <i class="fa-solid fa-star fa-2xs" style="color:#FDAF22;"></i>
 
-                <p class="px-md-5 text-center heroparagraph">
-                    Kokokah combines <strong>School Management</strong>, <strong>Exam Prep</strong>,
-                    and a <strong>Learning Management System (LMS)</strong>—helping <br>
-                    schools automate admin tasks, boost student performance, and deliver modern digital learning in one
-                    seamless platform.
-                </p>
-
-                <!-- Subscribe form -->
-                <form class="d-flex justify-content-center mt-4">
-                    <input type="email" class="form-control form-control-lg rounded-start-pill w-50"
-                        placeholder="Enter your email">
-                    <button type="submit" class="btn btn-lg rounded-end-pill px-4 text-white"
-                        style="background-color:#004d4d;">
-                        Subscribe
-                    </button>
-                </form>
-
-                <!-- Avatars + text -->
-                <div class="d-flex justify-content-center align-items-center gap-2 mt-4">
-                    <img src="https://i.pravatar.cc/40?img=1" class="rounded-circle border" width="40" height="40"
-                        alt="">
-                    <img src="https://i.pravatar.cc/40?img=2" class="rounded-circle border" width="40" height="40"
-                        alt="">
-                    <img src="https://i.pravatar.cc/40?img=3" class="rounded-circle border" width="40" height="40"
-                        alt="">
-                    <span class="text-muted small">Join 39k other creatives</span>
+                                    </div>
+                                    <p class="sms-hero-label">4.8 ( form 1243+ reviews)</p>
+                                </div>
+                                <div class="d-flex flex-column gap-2 align-items-center">
+                                    <h1 class="text-center text-white">The All-in-one Platform for Modern Education</h1>
+                                    <p class="text-white text-center">Streamline school administration, enhance
+                                        communication and
+                                        empower educators with a single, intuitive system.</p>
+                                </div>
+                            </div>
+                            <button class="sms-hero-btn">Start 7-days free trial</button>
+                        </div>
+                        <div class="d-flex align-items-center gap-2 justify-content-center">
+                            <div class="d-flex align-items-center gap-1"><i class="fa-solid fa-circle-check fa-2xs"
+                                    style="color:#fff;"></i><span class="tagline-text">500 Schools</span></div>
+                            <div class="d-flex align-items-center gap-1"><i class="fa-solid fa-circle-check fa-2xs"
+                                    style="color:#fff;"></i><span class="tagline-text">500 Schools</span></div>
+                            <div class="d-flex align-items-center gap-1"><i class="fa-solid fa-circle-check fa-2xs"
+                                    style="color:#fff;"></i><span class="tagline-text">500 Schools</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <img src="./images/sms-hero-img.jpg" alt=""
+                        style="border-radius:30px; width:100%; max-height:600px;">
                 </div>
             </div>
+        </section>
+        <section class="container-fluid px-3 px-lg-5 py-5 d-flex flex-column gap-3">
+            <h2 class="text-center sms-feature-title">Everything you need to manage your school</h2>
+            <div class="row g-3">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-column sms-feature-card sms-feature-card-accent h-100">
+                        <div class="sms-feature-icon-container sms-feature-icon-container-accent">
+                            <img src="./images/grade-feature-icon.png" alt="">
+                        </div>
+                        <div class="d-flex flex-column gap-3">
+                            <h5>Grade Management</h5>
+                            <p class="text-white">Lorem ipsum amet eget lectus nibh dignissim enim eros feugiat pellentesque
+                                in bibendum...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-column sms-feature-card h-100">
+                        <div class="sms-feature-icon-container">
+                            <img src="./images/attendance-feature-icon.png" alt="">
+                        </div>
+                        <div class="d-flex flex-column gap-3">
+                            <h5>Attendance Tracking</h5>
+                            <p style="color:#000F11;">Lorem ipsum amet eget lectus nibh dignissim enim eros feugiat
+                                pellentesque...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-column sms-feature-card h-100">
+                        <div class="sms-feature-icon-container">
+                            <img src="./images/communication-feature-icon.png" alt="">
+                        </div>
+                        <div class="d-flex flex-column gap-3">
+                            <h5>Communication Tools</h5>
+                            <p style="color:#000F11;">Lorem ipsum amet eget lectus nibh dignissim enim eros feugiat
+                                pellentesque...</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-column sms-feature-card h-100">
+                        <div class="sms-feature-icon-container">
+                            <img src="./images/signup-feature-icon.png" alt="">
+                        </div>
+                        <div class="d-flex flex-column gap-3">
+                            <h5>Sign Up</h5>
+                            <p style="color:#000F11;">Lorem ipsum amet eget lectus nibh dignissim enim eros feugiat
+                                pellentesque...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+        <section class="container-fluid px-3 px-lg-5 py-5">
+            <div class="row g-4">
+                <div class="col col-12 col-lg-5">
+                    <div><img src="./images/sms-reason-img.png" alt="" class="w-100 h-100"
+                            style=" max-height:700px;"></div>
+
+                </div>
+                <div class="col col-12 col-lg-7 d-flex flex-column gap-3 align-items-center">
+                    <div class="d-flex flex-column gap-2">
+                        <h3 class="sms-feature-title text-center">Why schools choose our system</h3>
+                        <p class="text-center">Our platform is designed to stramline workflows, enhance parent-teacher
+                            collaboration and provide data-driven insights for better decision-making.</p>
+                    </div>
+                    <div class="row g-4">
+
+                        <div class="col col-12 col-md-6 ">
+                            <div class="d-flex flex-column gap-3 sms-reason-card h-100">
+                                <div><img src="./images/reason-card-icon.png" alt=""></div>
+                                <div class="d-flex flex-column gap-2">
+                                    <h5 style="color: #230B34;">Save Time</h5>
+                                    <p style="color: #230B34;">Learn from industry leaders and academic experts in hands-on,
+                                        specialized workshops.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-12 col-md-6">
+                            <div class="d-flex flex-column gap-3 sms-reason-card h-100">
+                                <div><img src="./images/reason-card-icon.png" alt=""></div>
+                                <div class="d-flex flex-column gap-2">
+                                    <h5 style="color: #230B34;">Improve Communication</h5>
+                                    <p style="color: #230B34;">Learn from industry leaders and academic experts in hands-on,
+                                        specialized workshops.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-12 col-md-6">
+                            <div class="d-flex flex-column gap-3 sms-reason-card h-100">
+                                <div><img src="./images/reason-card-icon.png" alt=""></div>
+                                <div class="d-flex flex-column gap-2">
+                                    <h5 style="color: #230B34;">Boost Engagement</h5>
+                                    <p style="color: #230B34;">Learn from industry leaders and academic experts in
+                                        hands-on, specialized workshops.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-12 col-md-6">
+                            <div class="d-flex flex-column gap-3 sms-reason-card h-100">
+                                <div><img src="./images/reason-card-icon.png" alt=""></div>
+                                <div class="d-flex flex-column gap-2">
+                                    <h5 style="color: #230B34;">Data-Driven Insight</h5>
+                                    <p style="color: #230B34;">Learn from industry leaders and academic experts in
+                                        hands-on, specialized workshops.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- Swiper Container -->
+<div class="swiper mySwiper py-5" style="width: 100%;">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide d-flex flex-column gap-5 flex-lg-row">
+        <div class="d-flex flex-column align-items-center gap-2 my-auto">
+            <h2>4.8</h2>
+            <div class="d-flex align-items-center gap-1">
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;" ></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-regular fa-star fa-xs" style="color: #FFC700;"></i>
+            </div>
         </div>
+        <div class="d-flex flex-column gap-4 align-items-center align-items-lg-start">
+            <div><i class="fa-solid fa-quote-left fa-2xl"></i></div>
 
-        <div class = "d-flex justify-content-between">
-            <!-- Left illustration -->
-            <div class="col-2 d-none d-md-block" style = "margin-top:100px;">
-                <img src="images/tree.png" alt="Trees" class="img-fluid">
+            <div class="d-flex align-items-center gap-2">
+                <img src="./images/sms-testimonial-img.jpg" alt="" class="swiper-slide-img">
+                <div class="d-flex flex-column gap-1">
+                    <h6>Rizka Amalia</h6>
+                    <p>Penanya</p>
+                </div>
             </div>
-
-            <!-- Right illustration -->
-            <div class="col-2 d-none d-md-block">
-                <img src="images/sms-person.png" alt="Person" class="img-fluid">
-            </div>
-
+            <p >Saya sudah banyak mencari tentang pertanyaan yang
+saya cari saat ini, dan di Brainly Reborn ini saya mendapatkan jawaban yang akurat dan simple.</p>
         </div>
     </div>
+    <div class="swiper-slide d-flex flex-column gap-5 flex-lg-row">
+        <div class="d-flex flex-column align-items-center gap-2 my-auto">
+            <h2>4.8</h2>
+            <div class="d-flex align-items-center gap-1">
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;" ></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-regular fa-star fa-xs" style="color: #FFC700;"></i>
+            </div>
+        </div>
+        <div class="d-flex flex-column gap-4 align-items-center align-items-lg-start">
+            <div><i class="fa-solid fa-quote-left fa-2xl"></i></div>
+
+            <div class="d-flex align-items-center gap-2">
+                <img src="./images/sms-testimonial-img.jpg" alt="" class="swiper-slide-img">
+                <div class="d-flex flex-column gap-1">
+                    <h6>Rizka Amalia</h6>
+                    <p>Penanya</p>
+                </div>
+            </div>
+            <p >Saya sudah banyak mencari tentang pertanyaan yang
+saya cari saat ini, dan di Brainly Reborn ini saya mendapatkan jawaban yang akurat dan simple.</p>
+        </div>
+    </div>
+    <div class="swiper-slide d-flex flex-column gap-5 flex-lg-row">
+        <div class="d-flex flex-column align-items-center gap-2 my-auto">
+            <h2>4.8</h2>
+            <div class="d-flex align-items-center gap-1">
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;" ></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-solid fa-star fa-xs" style="color: #FFC700;"></i>
+                <i class="fa-regular fa-star fa-xs" style="color: #FFC700;"></i>
+            </div>
+        </div>
+        <div class="d-flex flex-column gap-4 align-items-center align-items-lg-start">
+            <div><i class="fa-solid fa-quote-left fa-2xl"></i></div>
+
+            <div class="d-flex align-items-center gap-2">
+                <img src="./images/sms-testimonial-img.jpg" alt="" class="swiper-slide-img">
+                <div class="d-flex flex-column gap-1">
+                    <h6>Rizka Amalia</h6>
+                    <p>Penanya</p>
+                </div>
+            </div>
+            <p >Saya sudah banyak mencari tentang pertanyaan yang
+saya cari saat ini, dan di Brainly Reborn ini saya mendapatkan jawaban yang akurat dan simple.</p>
+        </div>
+    </div>
+
+  </div>
+
+  <!-- arrows -->
+  <div class="swiper-button-next"><i class="fa-solid fa-angle-right" style="color: #888888;"></i></div>
+  <div class="swiper-button-prev"><i class="fa-solid fa-angle-left" style="color: #888888;"></i></div>
+</div>
+
+
+
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+    <script>
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    768: { slidesPerView: 1.5 },
+    1024: { slidesPerView: 2 }
+  }
+});
+
+
+    </script>
 @endsection
