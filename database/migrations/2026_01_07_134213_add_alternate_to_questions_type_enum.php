faa5109897b22a056911a802a6f54aadce24a992
tsuccess <taofeeq.muhammad22@gmail.com>
2026-01-07 14:47:01 +0100

    fixed adding quiz functionality

---
+++ database/migrations/2026_01_07_134213_add_alternate_to_questions_type_enum.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Modify the type column in questions table to include 'alternate' in the ENUM
+        if (DB::getDriverName() === 'sqlite') {
+            // SQLite doesn't support MODIFY, so we skip this for SQLite
+            // The column will be created with the correct enum in the initial migration
+        } else {
+            DB::statement("ALTER TABLE questions MODIFY COLUMN type ENUM('mcq', 'alternate', 'theory') DEFAULT 'mcq'");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (DB::getDriverName() === 'sqlite') {
+            // SQLite doesn't support MODIFY, so we skip this for SQLite
+        } else {
+            DB::statement("ALTER TABLE questions MODIFY COLUMN type ENUM('mcq', 'theory') DEFAULT 'mcq'");
+        }
+    }
+};
